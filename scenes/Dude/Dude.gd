bb498abdbae793482e281794e5c1501230b06c53
lucius <flyingrissole@gmail.com>
2023-11-16 22:46:41 +0300

    initialize

---
+++ scenes/Dude/Dude.gd
@@ -0,0 +1,23 @@
+extends CharacterBody2D
+
+enum Types {
+	Red,
+	Green,
+	Yellow,
+	Purple,
+}
+
+var TypeRects: Dictionary = {
+	Types.Red: 		Rect2(576, 320, 64, 64),
+	Types.Green: 	Rect2(576, 448, 64, 64),
+	Types.Yellow: 	Rect2(576, 256, 64, 64),
+	Types.Purple: 	Rect2(576, 384, 64, 64),
+}
+
+@onready var sprite: Sprite2D = $Sprite2D
+
+@onready var type: Types = Types.Red :
+	get:
+		return type
+	set(new_type):
+		sprite.set_region_rect(TypeRects[type])
