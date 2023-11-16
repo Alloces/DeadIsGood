6f0d32aab06b2ebbdfdad6b2714112e18bffcd1d
lucius <flyingrissole@gmail.com>
2023-11-16 23:42:25 +0300

    remove npc and player nodes to use only dude node

--- scenes/Dude/Dude.gd
+++ scenes/Dude/Dude.gd
@@ -1,8 +1,5 @@
-extends Node2D
+extends CharacterBody2D
 class_name Dude
-
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
 
 enum Types {
 	Red,
@@ -11,6 +8,33 @@ enum Types {
 	Purple,
 }
 
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+@onready var _sprite: Sprite2D = $Sprite2D
+@onready var _camera: Camera2D = $Camera2D
+
+@export var is_player: bool = false :
+	get:
+		return is_player
+	set(val):
+		is_player = val
+		if _camera == null:
+			return
+		
+		_camera.set_enabled(true)
+
+@export var type: Types = Types.Red :
+	get:
+		return type
+	set(new_type):
+		type = new_type
+		if _sprite == null:
+			return
+		
+		# TODO: check if type in dict
+		_sprite.set_region_rect(TypeRects[type])
+
 var TypeRects: Dictionary = {
 	Types.Red: 		Rect2(576, 320, 64, 64),
 	Types.Green: 	Rect2(576, 448, 64, 64),
@@ -18,28 +42,42 @@ var TypeRects: Dictionary = {
 	Types.Purple: 	Rect2(576, 384, 64, 64),
 }
 
-@export var _character: CharacterBody2D
-@onready var _sprite: Sprite2D = $Sprite2D
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-@onready var type: Types = Types.Red :
-	get:
-		return type
-	set(new_type):
-		# TODO: check if type in dict
-		type = new_type
-		_sprite.set_region_rect(TypeRects[type])
 
 func _ready() -> void:
-	if _character == null:
-		print("Dude -> ready: character must not be null")
-		return
+	_camera.enabled = is_player
+	_sprite.set_region_rect(TypeRects[type])
+
+
+func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	
+	if is_player:
+		_parse_input()
+	else:
+		_parse_npc_behavior()
+	
+	move_and_slide()
+
+
+func _parse_input() -> void:
+	if Input.is_action_just_pressed("ui_accept"):
+		use_ability()
+	
+	move(Input.get_axis("ui_left", "ui_right"))
+
+
+func _parse_npc_behavior() -> void:
+	pass
 
 
 func move(direction: float) -> void:
 	if direction:
-		_character.velocity.x = direction * SPEED
+		velocity.x = direction * SPEED
 	else:
-		_character.velocity.x = move_toward(_character.velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 
 func use_ability() -> void:
@@ -58,8 +96,8 @@ func _red_ability() -> void:
 
 
 func _green_ability() -> void:
-	if _character.is_on_floor():
-		_character.velocity.y = JUMP_VELOCITY
+	if is_on_floor():
+		velocity.y = JUMP_VELOCITY
 
 
 func _yellow_ability() -> void:
