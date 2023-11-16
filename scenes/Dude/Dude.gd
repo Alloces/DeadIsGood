df681f3ad7bd4b4ee5f8d85f668e9112e97cb140
lucius <flyingrissole@gmail.com>
2023-11-17 00:19:33 +0300

    improve dudes behavior

--- scenes/Dude/Dude.gd
+++ scenes/Dude/Dude.gd
@@ -1,6 +1,8 @@
 extends CharacterBody2D
 class_name Dude
 
+signal died(killer_name: String)
+
 enum Types {
 	Red,
 	Green,
@@ -13,6 +15,7 @@ const JUMP_VELOCITY = -400.0
 
 @onready var _sprite: Sprite2D = $Sprite2D
 @onready var _camera: Camera2D = $Camera2D
+@onready var _area: Area2D = $Area2D
 
 @export var is_player: bool = false :
 	get:
@@ -22,7 +25,7 @@ const JUMP_VELOCITY = -400.0
 		if _camera == null:
 			return
 		
-		_camera.set_enabled(true)
+		_change_is_player_state()
 
 @export var type: Types = Types.Red :
 	get:
@@ -46,8 +49,16 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
 func _ready() -> void:
-	_camera.enabled = is_player
+	_change_is_player_state()
 	_sprite.set_region_rect(TypeRects[type])
+
+
+func _change_is_player_state() -> void:
+	_camera.enabled = is_player
+	if is_player:
+		_area.connect("area_entered", _on_area_2d_area_entered)
+	elif _area.is_connected("area_entered", _on_area_2d_area_entered):
+		_area.disconnect("area_entered", _on_area_2d_area_entered)
 
 
 func _physics_process(delta: float) -> void:
@@ -106,3 +117,7 @@ func _yellow_ability() -> void:
 
 func _purple_ability() -> void:
 	print("purple ability")
+
+
+func _on_area_2d_area_entered(area: Area2D):
+	emit_signal("died", area.get_parent().name)
